c0cfaabe26461bd402f4caf098c3a3b72ffc0b1a
fairyroad <74306759+fairyroad@users.noreply.github.com>
2021-05-24 19:07:21 +0900

    Update week3_printer.cpp

--- week3_printer.cpp
+++ week3_printer.cpp
@@ -1,37 +1,47 @@
 #include <string>
 #include <vector>
 #include <queue>
-#include <algorithm>
-
+#include <iostream>
 using namespace std;
-
-int solution(vector<int> priorities, int location) {
-    int answer = 0;
-
-    queue<pair<int, int>> que;
-    priority_queue<int> pque;
-//priority queue, queue를 만들어서 vector값을 push시킴
-    for (int i = 0; i < priorities.size(); i++) {
-        //처음을 index번호, 두번째를 중요도로 넣음
-        que.push(make_pair(i, priorities[i]));
-        pque.push(priorities[i]);
+int find_max(queue<int> priority)
+{
+    int max = priority.front();
+    while(priority.size())
+    {
+        priority.pop();
+        if(max < priority.front())
+            max = priority.front();
     }
-
-    while (!que.empty()) {
-        //그냥 queue에서 하나씩 pop시켜서 priorityqueue의 pop시킨 값과 중요도가 같은지 확인
-        //중요도가 같더라도 내가 알고싶은 location인지를 check해야하기 때문에 그냥 queue도 이용하는것!!
-        if (que.front().second == pque.top()) {
-            if (que.front().first == location) {
-                return answer + 1;
-            }
-            else {
-                answer++;
-                que.pop(); pque.pop();
-            }
+    return max;
+}
+int solution(vector<int> priorities, int location) 
+{
+    int answer = 0;
+    queue<int> q;
+    queue<int> priority;
+    int num,loc;
+    //q에 priority넣음
+    for(int i=0;i<priorities.size();i++)
+    {
+        priority.push(priorities[i]);
+        q.push(i);
+    }
+    while(priority.size())
+    {
+        num = priority.front();
+        priority.pop();
+        loc = q.front();
+        q.pop();
+        if(num < find_max(priority))
+        {
+            priority.push(num);
+            q.push(loc);
         }
-        else {
-            que.push(que.front());
-            que.pop();
+        else
+        {
+            answer++;
+            if(loc == location)
+                return answer;
         }
     }
     return answer;
