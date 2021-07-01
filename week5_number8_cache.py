8c80430f9e9772191fb987ef42ea4ed9df50a9a9
fairyroad <74306759+fairyroad@users.noreply.github.com>
2021-07-02 00:33:46 +0900

    Update week5_number8_cache.py

--- week5_number8_cache.py
+++ week5_number8_cache.py
@@ -8,7 +8,7 @@
 #full이라면 기존에 있는 것중에 제일 처음것(index가 0인것)을 pop시키고 해당 도시를 push해주고
 #자리가 남아있다면 그냥 바로 push해주는 방식으로 구현함
 
-
+#----------------------------내가 구현한 방법--------------------------------
 def solution(cacheSize, cities):
     answer = 0
     queue = []
@@ -33,3 +33,24 @@ def solution(cacheSize, cities):
             queue.pop(queue.index(city))
             queue.append(city)      
     return answer
+
+#--------------------------좋은 풀이------------------------------
+#collections.deque()는 순환이나 프로세싱 중 마지막으로 발견한 N개의 아이템 유지하고 싶을 때 사용한다고 함 -> 우리문제에 적합함
+#deque를 이용하는데 maxlen을 설정할 수 있음!!
+#만약 cache에 존재하면 우선순위를 바꿔줘야하니까 지우고 다시 append시켜주고(내가 한 방법이랑 같음)
+#만약 cache에 존재하지 않는다면 바로 append시켜주면 됨
+#기존에 내 방식을 이용하면 크기에 따라 4가지 경우로 분리해야한다는 단점이 있었는데 collections.dequeue를 이용하게 되면 자동으로 최신것만 maxlen만큼 유지해주니까 삭제하지 않아도 된다는 장점이 있음
+def solution(cacheSize, cities):
+    import collections
+    cache = collections.deque(maxlen=cacheSize)
+    time = 0
+    for i in cities:
+        s = i.lower()
+        if s in cache:
+            cache.remove(s)
+            cache.append(s)
+            time += 1
+        else:
+            cache.append(s)
+            time += 5
+    return time
