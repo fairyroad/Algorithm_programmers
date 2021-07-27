0e49a75adfc5e35cd9f0a830bc82e01b90f3d4a5
Yujung Gil <74306759+fairyroad@users.noreply.github.com>
2021-07-27 18:16:15 +0900

    Update week8_number1.cpp

--- week8_number1.cpp
+++ week8_number1.cpp
@@ -15,8 +15,6 @@ using namespace std;
 long long solution(int w,int h) {
     long long answer = 0;
     for(int i=1;i<w;i++)
-    {
         answer+=(long long)((long double)(-h/(long double)w)*i+h);
-    }
     return answer*2;
 }
